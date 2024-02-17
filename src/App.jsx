bbc2220cc21e142e4db906571a7f0f85aade9d19
jvk <jvkkoppineedi@gmail.com>
2024-02-17 19:48:28 +0530

    added status messages of the screen shot

--- src/App.jsx
+++ src/App.jsx
@@ -11,33 +11,35 @@ function App() {
   const [isLoading, setIsLoading] = useState(false)
   const [error, setError] = useState(null)
   const [imageLoaded, setImageLoaded] = useState(false)
+  const [statusMessage, setStatusMessage] = useState("")
 
   const getSS = async () => {
     const query = "https://api.screenshotone.com/take?format=jpeg&image_quality=5&full_page=true"
     const url = encodeURIComponent(urlRef.current.value)
     setIsLoading(true)
     setImageLoaded(false)
+    setStatusMessage("")
     setError(null)
     try {
+      setStatusMessage("Getting Screenshot")
       const res1 = await axios.get(`${query}&url=${url}&access_key=${import.meta.env.VITE_SCREENSHOTONE_ACCESS_KEY}`, { responseType: 'arraybuffer' })
-      console.log(res1)
       const base64Img = btoa(
         new Uint8Array(res1.data).reduce(
           (data, byte) => data + String.fromCharCode(byte),
           '',
         ),
       );
-
+      setStatusMessage("Uploading to Cloudinary")
       const res2 = await axios.post(`https://api.cloudinary.com/v1_1/${import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}/image/upload`, {
         file: `data:image/jpeg;base64,${base64Img}`,
         upload_preset: `${import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET}`
       })
-      console.log(res2)
       setImage(res2.data)
     } catch (err) {
       setError(err)
     }
     setIsLoading(false)
+    setStatusMessage("")
   }
   return (
     <>
@@ -71,30 +73,35 @@ function App() {
           }
         </div>
         <div>
-          {image == "" && !isLoading && <Intro />}
-          <ClockLoader
-            color={"#000000"}
-            loading={isLoading}
-            size={50}
-            aria-label="Loading Spinner"
-            data-testid="loader"
-          />
-          {error && <div>{error.message}</div> }
+          <div className="flex flex-col items-center">
+            {image == "" && !isLoading && <Intro />}
+            <ClockLoader
+              color={"#000000"}
+              loading={isLoading}
+              size={50}
+              aria-label="Loading Spinner"
+              data-testid="loader"
+            />
+            {statusMessage != "" &&
+              <div>{statusMessage}</div>
+            }
+          </div>
+          {error && <div>{error.message}</div>}
           {image != "" && isLoading == false && error == null &&
             <div
               className="flex flex-col p-5 border-2 w-fit m-5 shadow-lg rounded-lg bg-gray-100 font-serif"
             >
               <SyncLoader loading={!imageLoaded} />
-              {imageLoaded && 
+              {imageLoaded &&
                 <div
                   className="flex flex-col border-b-2 m-3"
                 >
-                    <span>Created at: {image.created_at}</span>
-                    <span>Size: {Math.round(image.bytes / 1024, 3)} Kb</span>
+                  <span>Created at: {image.created_at}</span>
+                  <span>Size: {Math.round(image.bytes / 1024, 3)} Kb</span>
                 </div>
               }
               <div>
-                <Image className={"shadow-lg rounded-lg"} cloudName="duznb5jsl" publicId={image.public_id} onLoad={()=> setImageLoaded(true)} />
+                <Image className={"shadow-lg rounded-lg"} cloudName="duznb5jsl" publicId={image.public_id} onLoad={() => setImageLoaded(true)} />
               </div>
 
             </div>
